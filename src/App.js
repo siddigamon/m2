c37964e3364f8376310b93cf9fc2559a93e57735
siddigamon <115434776+siddigamon@users.noreply.github.com>
2024-03-03 22:44:49 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Employees from './pages/Employees';
+import Add from './pages/Add';
+import Update from './pages/Update';
+import './App.css';
+import './Employee.css';
+import View from './pages/View'; 
+
+
+
+
+function App() {
+  return (
+    <div className="App" >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Employees/>} />
+          <Route path="/add" element={<Add/>} />
+          <Route path="/update/:id" element={<Update/>} />
+          <Route path="/view" element={<View/>} /> 
+         
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+     
+  );
+}
+
+export default App;
